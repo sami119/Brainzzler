f7254e4bc3c400a112d00de20e0daf5558ebb0dd
Samuil <35292702+sami119@users.noreply.github.com>
2019-02-17 07:56:31 +0200

    Revert "Deleted files"
    This reverts commit a4a4f8b091b0feecdb658204fde9e29775d4e149.

--- Brainzzler/Brainzzler/Startup.cs
+++ Brainzzler/Brainzzler/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -40,6 +40,7 @@ namespace Brainzzler
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -53,7 +54,7 @@ namespace Brainzzler
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
 
@@ -63,12 +64,7 @@ namespace Brainzzler
 
             app.UseAuthentication();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
         }
     }
 }
