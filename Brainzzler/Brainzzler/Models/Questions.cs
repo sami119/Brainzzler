3a63c5b7c1fc4fb9e51a8027e078b4065df043e4
elica lazarova <elica.lazarowa@gmail.com>
2019-02-24 10:42:06 +0200

    DB & Models Created

---
+++ Brainzzler/Brainzzler/Models/Questions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Brainzzler.Models
+{
+    public partial class Questions
+    {
+        public long Id { get; set; }
+        public string Question { get; set; }
+    }
+}
