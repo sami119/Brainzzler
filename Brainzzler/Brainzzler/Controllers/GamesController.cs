17ebf6af1693a008b156607f6480ae5b5fb937f5
Samuil <35292702+sami119@users.noreply.github.com>
2019-02-27 18:26:08 +0200

    Games-Update
    Work on:
    -Documentation
    -Games-slider
    -games-iframe
    Todo:
    Work on games slider layout
    -buisness logic
    -games
    
    Co-Authored-By: eli-lazarova <eli-lazarova@users.noreply.github.com>

--- Brainzzler/Brainzzler/Controllers/GamesController.cs
+++ Brainzzler/Brainzzler/Controllers/GamesController.cs
@@ -12,5 +12,10 @@ namespace Brainzzler.Controllers
         {
             return View();
         }
+
+        public IActionResult TwoZeroFourEight()
+        {
+            return View();
+        }
     }
 }
