245d75f6eba06963028691a99172e5a69df59515
elica lazarova <elica.lazarowa@gmail.com>
2019-04-03 20:21:35 +0300

    AnswerSheetsControllerTests

---
+++ Brainzzler/LogicTests/AnswerSheetsControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Brainzzler.Controllers;
+using Brainzzler.Models;
+using NUnit.Framework;
+using Moq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tests
+{
+    public class AnswerSheetsControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AnswerSheetsControllerReturnsNoContent()
+        {
+            var controller = new AnswerSheetsController();
+            var AnswerSheetsResult = controller.Index() as ViewResult;
+
+            Assert.AreEqual(NoContentResult, AnswerSheetsResult.ViewName);
+
+        }
+    }
+}
