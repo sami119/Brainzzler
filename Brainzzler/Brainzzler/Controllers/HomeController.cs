6b8803174f3f77a5c8fb86fa3e649c58790e7281
elica lazarova <elica.lazarowa@gmail.com>
2019-04-03 19:28:14 +0300

    Unit Tests Update 3
    Co-Authored-By: Samuil <sami119@users.noreply.github.com>

--- Brainzzler/Brainzzler/Controllers/HomeController.cs
+++ Brainzzler/Brainzzler/Controllers/HomeController.cs
@@ -13,12 +13,12 @@ namespace Brainzzler.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            return View("Privacy");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Brainzzler/LogicTests/HomeControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Brainzzler.Controllers;
+using Brainzzler.Models;
+using NUnit.Framework;
+using Moq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tests
+{
+    public class HomeControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void HomeControllerReturnsIndexView()
+        {
+            var controller = new HomeController();
+            var homeResult = controller.Index() as ViewResult;
+
+            Assert.AreEqual("Index", homeResult.ViewName);
+
+        }
+
+        [Test]
+        public void HomeControllerReturnsPrivacyView()
+        {
+
+            var controller = new HomeController();
+            var homeResult = controller.Privacy() as ViewResult;
+
+            Assert.AreEqual("Privacy", homeResult.ViewName);
+        }
+
+        /*[Test]
+        public void HomeControllerReturnsErrorView()
+        {
+
+            var controller = new HomeController();
+            var homeResult = controller.Error() as ViewResult;
+
+            Assert.AreEqual(new ErrorViewModel(), homeResult.ViewName);
+        }*/
+    }
+}
